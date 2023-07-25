df413d60ef3a9a8dd1161e32b9e1c8d78bcd375e
alissonsdr <alisson_sr@outlook.com>
2023-07-24 22:17:02 -0300

    project complet

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 let cars = []
 const resultCars = document.getElementById("resultCars")
 const resultPay = document.getElementById("resultPay")
+const date = new Date();
+const currentYear = date.getFullYear();
 
 function save() {
+
+    resultCars.innerHTML = ""
+    resultPay.innerHTML = ""
+    cars = []
     let brand = document.getElementById("brand").value
     let model = document.getElementById("model").value
     let color = document.getElementById("color").value
     let year = document.getElementById("year").value
-
     cars.push({
         carBrand: brand,
         carModel: model,
         carColor: color,
         carYear: year
     })
-    console.log(cars)
+
 }
 
 function view() {
 
     let listCars = ''
-
     cars.forEach(car => {
         listCars += `<span> ${car.carBrand}/${car.carModel} - ${car.carColor} - ${car.carYear} </span> <br>`
     })
-
     resultCars.innerHTML = listCars
+
 }
 
 function reset(){
+
     resultCars.innerHTML = ""
     resultPay.innerHTML = ""
     cars = []
+
 }
 
 function verify(){
@@ -39,13 +45,12 @@ function verify(){
     let listPay = ""
 
     cars.forEach(car => {
-        let yearCount = 2023 - `${car.carYear}`
+        let yearCount = currentYear - `${car.carYear}`
         if (yearCount >= 20){
             listPay += `${car.carModel}/${car.carYear} não paga IPVA. <br>`
         }
 
         resultPay.innerHTML = listPay
     })
-
 
 }
